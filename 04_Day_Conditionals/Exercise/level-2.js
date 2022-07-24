08093875e6e27b00c84e84c3deeb445a13931a40
harveyywang <harveyywang@tencent.com>
2022-07-24 21:26:39 +0800

    add: 完成第4天学习,完成课后习题

---
+++ 04_Day_Conditionals/Exercise/level-2.js
@@ -0,0 +1,81 @@
+// Exercises: Level 2
+// 1. Write a code which can give grades to students according to theirs scores:
+// 80-100, A
+// 70-89, B
+// 60-69, C
+// 50-59, D
+// 0-49, F
+let score = 90
+let grades
+if(score>80){
+    grades = 'A'
+}else if(score>70){
+    grades = 'B'
+}else if(score>60){
+    grades = 'C'
+}else if(score>50){
+    grades = 'D'
+}else{
+    grades = 'F'
+}
+
+// 2. Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+// September, October or November, the season is Autumn.
+// December, January or February, the season is Winter.
+// March, April or May, the season is Spring
+// June, July or August, the season is Summer
+let season = prompt("Enter season:")
+switch(season){
+    case 'September':
+    case 'October':
+    case 'November':
+        console.log('the season is Autumn')
+        break
+    case 'December':
+    case 'January':
+    case 'February':
+        console.log('the season is Winter')
+        break
+    case 'March':
+    case 'April':
+    case 'May':
+        console.log('the season is Spring')
+        break
+    case 'June':
+    case 'July':
+    case 'August':
+        console.log('the season is Summer')
+        break
+    default:
+        console.log('please input right season')
+}
+// 3. Check if a day is weekend day or a working day. Your script will take day as an input.
+//     What is the day  today? Saturday
+//     Saturday is a weekend.
+
+//     What is the day today? saturDaY
+//     Saturday is a weekend.
+
+//     What is the day today? Friday
+//     Friday is a working day.
+
+//     What is the day today? FrIDAy
+//     Friday is a working day.
+let today = prompt("What is the day today? ")
+let todayUpper = today.toLowerCase().replace(/^\S/g,function(s){return s.toUpperCase()})
+console.log(todayUpper)
+switch (todayUpper){
+    case 'Monday':
+    case 'Tuesday':
+    case 'Wednesday':
+    case 'Thursday':
+    case 'Friday':
+        console.log(`${todayUpper} is a working day`)
+        break
+    case 'Saturday':
+    case 'Sunday':
+        console.log(`${todayUpper} is a weekend`)
+        break
+    default:
+        console.log('please input a right day')
+}
---
+++ 04_Day_Conditionals/Exercise/level-3.js
@@ -0,0 +1,33 @@
+// Exercises: Level 3
+// Write a program which tells the number of days in a month.
+//   Enter a month: January
+//   January has 31 days.
+
+//   Enter a month: JANUARY
+//   January has 31 day
+
+//   Enter a month: February
+//   February has 28 days.
+
+//   Enter a month: FEbruary
+//   February has 28 days.
+// Write a program which tells the number of days in a month, now consider leap year.
+let month = prompt('Enter a month: ')
+month = month.toLowerCase().replace(/^\S/g,function(s){return s.toUpperCase()})
+let monthObi = {
+    "January": 1,
+    "February": 2,
+    "March": 3,
+    "April": 4,
+    "May": 5,
+    "June": 6,
+    "July": 7,
+    "August": 8,
+    "September": 9,
+    "October": 10,
+    "November": 11,
+    "December": 12
+
+}
+const date = new Date(2022,monthObi[month],0).getDate();
+console.log(`${month} has ${date} days`)
